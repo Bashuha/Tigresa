9105fe6902b4df93c68ee7345e8be5e13de6bdf4
Илья Боярский <boyailya@gmail.com>
2024-10-15 16:47:24 +0300

    start to do fsm

--- sets/functions.py
+++ sets/functions.py
@@ -4,32 +4,36 @@ import database.schemas as db_schema
 from sqlalchemy import insert, select
 from aiogram import types
 from database.engine import bot
+from aiogram.fsm.state import  StatesGroup, State
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+
+
+def make_row_keyboard(items: list[str]) -> ReplyKeyboardMarkup:
+    """
+    Создаёт реплай-клавиатуру с кнопками в один ряд
+    :param items: список текстов для кнопок
+    :return: объект реплай-клавиатуры
+    """
+    # переделать так, чтобы список кнопок был бесконечный
+    # и их можно было листать  
+    row = [KeyboardButton(text=item) for item in items]
+    return ReplyKeyboardMarkup(keyboard=[row], resize_keyboard=True)
+
+
+def valid_csv(document):
+    if document.mime_type != 'text/csv':
+        return "Некорректный формат файла! Поддерживается только CSV."
+        
+
 
 
 async def check_csv(
+    document,
     message: types.Message,
     session: AsyncSession,
 ):
-    tg_user_id = message.from_user.id
-    file_id = message.document.file_id
-    file_data = await bot.get_file(file_id)
-    file_path = file_data.file_path
-    await bot.download_file(file_path, "temp.csv")
-    
-    if message.document.mime_type != 'text/csv':
-        await message.reply("Некорректный формат файла! Поддерживается только CSV.")
-        return
-    
-    csv_value = list()
-    with open('temp.csv', newline='') as csvfile:
-        word_reader = csv.reader(
-            csvfile,
-            # delimiter='',
-            # quotechar='|'
-        )
-        for row in word_reader:
-            csv_value.append(row)
-
+    # потом проверим и добавим (если надо) пользователя
+    tg_user_id = document.from_user.id
     check_user = await session.execute(
         select(db_schema.User.id).
         where(db_schema.User.tg_id == tg_user_id)
@@ -43,10 +47,40 @@ async def check_csv(
         user_id = insert_user.inserted_primary_key[0]
         await session.commit()
 
+    # проверить существование сета с текущим названием
+    file_name = document.file_name[:-4]
+    check_set_name_query = await session.execute(
+        select(db_schema.SetName.id).
+        where(
+            db_schema.SetName.name == file_name,
+            db_schema.SetName.user_id == user_id
+        )
+    )
+    check_set_name = check_set_name_query.scalar_one_or_none()
+    if check_set_name:
+        await message.reply("У вас уже есть набор слов с таким названием")
+        return
+    
+    # дальше обработаем данные файла
+    file_id = message.document.file_id
+    file_data = await bot.get_file(file_id)
+    file_path = file_data.file_path
+    await bot.download_file(file_path, "temp.csv")
+    csv_value = list()
+    with open('temp.csv', newline='') as csvfile:
+        word_reader = csv.reader(
+            csvfile,
+            # delimiter='',
+            # quotechar='|'
+        )
+        for row in word_reader:
+            csv_value.append(row)
+
+
     insert_set_name = await session.execute(
         insert(db_schema.SetName).
         values(
-            name='test_csv',
+            name=file_name,
             user_id=user_id,
         )
     )
@@ -69,3 +103,12 @@ async def check_csv(
     await session.close()
     
     await message.reply("CSV успешно проверен!")
+
+
+class Challenge(StatesGroup):
+    choose_set = State()
+    enter_word = State()
+
+
+class SendCSV(StatesGroup):
+    sending_csv = State()
--- sets/router.py
+++ sets/router.py
@@ -1,18 +1,62 @@
 __all__ = ("router", )
 
+from magic_filter import F
 from database.engine import DBFilter
-from aiogram import Router
+from aiogram import Router, F
 from aiogram import types
 from sqlalchemy.ext.asyncio import AsyncSession
 from sets.functions import check_csv
+from aiogram.filters import Command, StateFilter
+from aiogram.fsm.context import FSMContext
+from sets.functions import Challenge, SendCSV
+from sets.functions import make_row_keyboard
 
 
 router = Router()
 
 
-@router.message(DBFilter())
+@router.message(
+    SendCSV.sending_csv,
+    F.document.cast().as_('document'),   
+    DBFilter(),
+)
 async def handle_file(
+    document,
     message: types.Message,
     session: AsyncSession,
+    state: FSMContext,
 ):
-    await check_csv(message, session)
+    await check_csv(document, message, session)
+    await state.clear()
+
+
+@router.message(StateFilter(None), Command('add_set'))
+async def handle_add_set_command(
+    message: types.Message,
+    state: FSMContext,
+):
+    await message.answer(
+        text='Отправьте мне CSV файл с вашими словами'
+    )
+    await state.set_state(SendCSV.sending_csv)
+
+
+@router.message(SendCSV.sending_csv)
+async def incorrect_message_sended(message: types.Message):
+    await message.answer(
+        text="Мне кроме CSV ничего не надо сейчас, попробуйте еще разок"
+    )
+
+
+@router.message(StateFilter(None), Command('challenge'))
+async def start_challenge(
+    message: types.Message,
+    state: FSMContext,
+):
+    # здесь будет запрос на получение сетов пользователя
+    user_sets = [1,2,3,4]
+    await message.answer(
+        text="Выберите, из какого набора сделать тест",
+        reply_markup=make_row_keyboard(user_sets)
+    )
+    await state.set_state(Challenge.choose_set)
